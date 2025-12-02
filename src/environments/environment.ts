fed238105999f52fc2c536c1d912614800f18130
juani <contactosjuandm@gmail.com>
2025-12-02 11:50:15 -0700

    fix: actualiza imports y añade template seguro para API keysA

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  
+  // Tus valores REALES de Firebase aqu�:
+  firebaseConfig: {
+    apiKey: "AIzaSyD7Nyn83lzXCb3ISWZ4TjT2yd15SkIylEY",
+    authDomain: "chatbot-app10a.firebaseapp.com",
+    projectId: "chatbot-app10a",
+    storageBucket: "chatbot-app10a.firebasestorage.app",
+    messagingSenderId: "960881026186",
+    appId: "1:960881026186:web:11aa629feb93acd5117973"
+  },
+  
+  // Tu API Key de Gemini para desarrollo (usa placeholder si quieres subir a GitHub)
+geminiApiKey: "AIzaSyDRoCA-gmXadwvtSudpKy08WBYBI7vDaZk"
+};
