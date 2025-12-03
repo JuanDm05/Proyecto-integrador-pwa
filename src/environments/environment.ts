8a401a5b11d6cb23154f62da30f7fe18dd047b54
DirierSebastian <sebastianagvzla@gmail.com>
2025-12-02 18:17:15 -0700

    gitignore arreglado y vuelta de environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD7Nyn83lzXCb3ISWZ4TjT2yd15SkIylEY",
+    authDomain: "chatbot-app10a.firebaseapp.com",
+    projectId: "chatbot-app10a",
+    storageBucket: "chatbot-app10a.firebasestorage.app",
+    messagingSenderId: "960881026186",
+    appId: "1:960881026186:web:11aa629feb93acd5117973"
+  },
+
+
+  geminiApiKey: "GEMINI_API_KEY_TO_BE_REPLACED"
+};
+
